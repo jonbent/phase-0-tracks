7719c5b5f08179ba6e317a683b66fbed04ee1d33
Jon Bent <jon.bent@me.com>
2016-11-26 09:19:04 -0800

    completed all of the challenge

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,58 @@
+# get spy's name
+
+# split spy's name, reverse it, then join it back together, might be messy use irb first.
+# define encrypting method
+# inside, define consonates and vowels methods.
+
+
+def encrypt
+	quit = 0
+	fake_names = []
+	puts "spy's name? Type quit to end"
+	until quit == 'quit'
+		spys_name = gets.chomp
+		quit = spys_name
+		spys_name_length = spys_name.length
+		spys_name = spys_name.split.reverse
+		spys_name = spys_name.join(' ').split('')
+		vowels = ['a', 'e', 'i', 'o', 'u']
+		consonates = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+		x = 0
+		number_vowels = 5
+		while spys_name_length > 0
+			spys_name[x] = spys_name[x].downcase
+			if quit == "quit"
+				break
+			elsif vowels.include?(spys_name[x])
+				vowel_position = vowels.index(spys_name[x])
+				vowel_position += 1
+				if vowel_position == 5
+					vowel_position = 0
+			end
+			spys_name[x] = vowels[vowel_position]
+				
+			elsif consonates.include?(spys_name[x])
+				consonate_position = consonates.index(spys_name[x])
+				consonate_position += 1
+				if consonate_position == 20
+					consonate_position = 0
+			end
+			spys_name[x] = consonates[consonate_position]
+			elsif spys_name == ' '
+				spys_name = ' '
+				# keep it that way
+			end
+			x += 1
+			spys_name_length -= 1
+		end
+		spys_name = spys_name.join.split(' ').each {|word| word.capitalize!}
+		spys_name = spys_name.join(" ")
+		fake_names << spys_name
+	end
+	fake_names.delete("Quit")
+	fake_names
+end
+
+
+encrypt
+
