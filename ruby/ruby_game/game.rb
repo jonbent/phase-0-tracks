5ea41c6b3138236653cee1740581c8f8b5a724fc
Jon Bent <jon.bent@me.com>
2016-12-04 18:44:49 -0800

    moved all loops and logic inside the class

--- ruby/ruby_game/game.rb
+++ ruby/ruby_game/game.rb
@@ -1,33 +1,82 @@
 # game class
 class Hangman
-	attr_accessor :current_guess, :guess
-	def initialize(input, empty_answer, empty_answer_array, answer_array)
-		@input = input
-		@current_guess = empty_answer
-		@empty_answer_array = empty_answer_array
-		@answer_array = answer_array
+	attr_reader :empty_answer_array
+	def initialize(word)
+		@guess_list = []
+		@word = word
+		@empty_answer_array = []
+		@word.length.times do
+			@empty_answer_array << "_"
+		end
+		@answer_array = word.split('')
+		@current_guess = empty_answer_array.join('')
+		@guesses = 6
 	end
   	def progress
     	p @current_guess
   	end
   	def lose_game
     	puts "wow you lost... you're pathetic"
+    	puts "These were your guesses: #{@guess_list.join(", ")}"
   	end
   
   	def win_game
    		puts "You guessed the word! You're so cool!"
+   		puts "These were your guesses: #{@guess_list.join(", ")}"
   	end
   
   	def switch_duplicates
-    	@input.length.times do |letter|
+    	@word.length.times do |letter|
      		if @guess == @answer_array[letter]
           		@empty_answer_array[letter] = @guess
       		end
     	end
   	end
-  
-  	def end_game
-   		true
+
+  	def correct_letter
+  		if @answer_array.include?(@guess)
+	    	switch_duplicates
+	    	@current_guess = @empty_answer_array.join(" ")
+	    	progress
+	    
+	    else
+	    	@guesses -= 1
+	    	puts "WRONG! you have #{@guesses} guesses left"
+	    	@current_guess = @empty_answer_array.join(" ")
+	    	progress
+	    end
+  	end
+
+  	def fix_current_guess
+  		fixed = @current_guess.split(' ')
+  		fixed.join('')
+
+  	end
+  	def true_or_false
+	  	if @guess == @word
+	    	@guesses = 0
+	  	elsif !@guess_list.include?(@guess)
+	    	@guess_list << @guess
+	    	correct_letter
+		else 
+	    	puts "well you already entered that, so try again."
+	    	@current_guess
+	  	end
+  	end
+  	def win_or_lose
+  		if fix_current_guess == @word || @guess == @word
+		  	win_game
+		else
+		  	lose_game
+		  	p current_guess
+		end
+  	end
+
+  	def game
+  		until @guesses == 0 || fix_current_guess == @word
+			@guess = gets.chomp
+  			true_or_false
+  		end
   	end
 end
 # user interface
@@ -35,42 +84,8 @@ end
 puts "Welcome to hangman, please insert word for partner to guess"
 word = gets.chomp
 print "\e[2J\e[f"
-empty_answer_array = []
-word.length.times do
-	empty_answer_array << "_"
-end
-empty_answer = empty_answer_array.join('')
-answer_array = word.split('')
-guesses = 6
 puts "Please hand me to your partner now"
 puts "Welcome to hangman, you get 6 guesses and you have to guess my word!"
-game = Hangman.new(word, empty_answer, empty_answer_array, answer_array)
-guess_list = []
-position = []
-until guesses == 0 || empty_answer_array.join('') == word
-	game.guess = gets.chomp
-  	if game.guess == word
-    	guesses = 0
-  	elsif !guess_list.include?(game.guess)
-    	guess_list << game.guess
-	    if answer_array.include?(game.guess)
-	    	game.switch_duplicates
-	    	game.current_guess = empty_answer_array.join(" ")
-	    	game.progress
-	    
-	    else
-	    	guesses -=1
-	    	puts "WRONG! you have #{guesses} guesses left"
-	    	game.progress
-	    end
-	else 
-    	puts "well you already entered that, so try again."
-    	game.current_guess
-  	end
-end
-
-if empty_answer_array.join('') == word || game.guess == word
-  	game.win_game
-else
-  	game.lose_game
-end
+game = Hangman.new(word)
+game.game
+game.win_or_lose
