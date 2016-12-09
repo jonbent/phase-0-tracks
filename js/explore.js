2f6b6bbda016509a983e697b22aa782933ecf240
Jon <jon.bent@me.com>
2016-12-08 16:18:52 -0800

    added function switcheroo

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,12 @@
 // input: string
 // steps: change to array, reverse elements in array, revert to string
 // output: string
+
+function switcheroo(str)
+{
+	for (var i = str.length; i > -1; i--){
+		console.log(str[i]);
+	}
+}
+
+switcheroo("hello")
