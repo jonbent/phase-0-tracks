b97e9e3cc10a9d8d7c6d488caba7351e22afec41
Jon <jon.bent@me.com>
2016-12-09 00:30:15 -0800

    made into function, with driver code. fixed into single line output

--- js/explore.js
+++ js/explore.js
@@ -2,11 +2,26 @@
 // steps: change to array, reverse elements in array, revert to string
 // output: string
 
-function switcheroo(str)
-{
-	for (var i = str.length; i > -1; i--){
-		console.log(str[i]);
-	}
-}
+f// input: string
+// steps: change to array, reverse elements in array, revert to string
+// output: string
 
-switcheroo("hello")
+function switcheroo(string){
+  var switched_word = "";
+	for (var i = string.length; i > -1; i--){
+		letter = string[i];
+		switched_word += letter;
+	}
+	return switched_word.replace("undefined", "");
+}
+// i can't for the life of my find out why it says 'undefined' when i output the 'switched_word' variable, I initialized it and everything, it just adds that undefined no matter what i do."
+switched = switcheroo("hello");
+
+if (switched == "olleh"){
+  console.log(switched)
+  console.log("wow that matches.");
+}
+else{
+  console.log(switched)
+  console.log("that doesn't match");
+}
