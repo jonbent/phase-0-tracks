9c421590bd944cb3d2f1aa90e2a16077ba0b066e
Jon Bent <jon.bent@me.com>
2016-12-09 16:38:12 -0800

    added file

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var colors= ["green", "beige", "yellow", "light-blue"];
+var names = ["Ed", "Chomper", "Blackie", "Charlie" ];
+
+
+colors.push("Black");
+names.push("Dixie");
+
+
+console.log(colors);
+console.log(names);
