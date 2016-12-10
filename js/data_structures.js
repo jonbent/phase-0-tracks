ce2e32b88b95c36882a1d1b018a04d8bfe27ecef
Jon Bent <jon.bent@me.com>
2016-12-09 17:15:18 -0800

    added car function, and all it's filler, added driver code.

--- js/data_structures.js
+++ js/data_structures.js
@@ -12,3 +12,25 @@ for (var i = 0; i < names.length; i++){
 console.log(colors);
 console.log(names);
 console.log(horse_colors);
+
+function Car(horsepower, torque, convertable) {
+  
+  console.log("Our new car:", this);
+  
+  this.horsepower = horsepower;
+  this.torque = torque;
+  this.convertable = convertable;
+
+  this.vroom = function() { console.log("*vroom-vroom*");};
+  
+  
+  console.log("car initialization complete!!");
+}
+
+var new_car = new Car(375, 300, true);
+console.log("this is the the car's horsepower: " + new_car.horsepower + ", and here is it's torque: " + new_car.torque);
+if (new_car.convertable) {
+  console.log("This is a convertable!");
+}
+
+new_car.vroom();
