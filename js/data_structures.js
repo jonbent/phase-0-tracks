054831a7cde78e4f121e24d393e4b7ec768dd20d
Jon Bent <jon.bent@me.com>
2016-12-09 16:51:05 -0800

    added horse color object and logic to fill it

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,10 +1,14 @@
 var colors= ["green", "beige", "yellow", "light-blue"];
 var names = ["Ed", "Chomper", "Blackie", "Charlie" ];
 
-
+var horse_colors = {};
 colors.push("Black");
 names.push("Dixie");
 
+for (var i = 0; i < names.length; i++){
+  horse_colors[names[i]] = colors[i];
+}
 
 console.log(colors);
 console.log(names);
+console.log(horse_colors);
