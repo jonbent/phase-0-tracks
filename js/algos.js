1ff7f51df3f231495b6fc5e0501783294c75c763
Jon Bent <jon.bent@me.com>
2016-12-11 02:45:39 -0800

    added sharing_is_caring function, and driver code.

--- js/algos.js
+++ js/algos.js
@@ -20,3 +20,29 @@ longest_program.choose_longest();
 array = ["123", "12345", "1234", "1"]
 longest_program = new Longest(array);
 longest_program.choose_longest();
+
+
+function sharing_is_caring(object_1, object_2) {
+  this.object_1 = object_1;
+  this.object_2 = object_2;
+	
+	this.check_objects = function(){
+	  if (this.object_1.age == this.object_2.age){
+	    return true;
+	  }else if(this.object_1.name == this.object_2.name){
+	    return true;
+	  }else{
+	    return false;
+	  }
+	 
+	};
+}
+
+object_checker = new sharing_is_caring({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+object_checker.check_objects();
+
+object_checker = new sharing_is_caring({name: "Steven", age: 26}, {name: "Tamir", age: 54});
+object_checker.check_objects();
+
+object_checker = new sharing_is_caring({name: "Steven", age: 26}, {name: "Steven", age: 54});
+object_checker.check_objects();
